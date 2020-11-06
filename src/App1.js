b9a24fcd1d81dc0010efe30fd881957c6c956be3
annie2527 <you@example.com>
2020-11-06 14:07:48 +0100

    css

--- src/App1.js
+++ src/App1.js
@@ -1,12 +1,104 @@
-import React from 'react'
-import './App.css';
+// import React, { useState, useEffect } from 'react'
+// import { DateTime, datetimeFromREST  } from 'luxon';
+// import './App.css';
 
-function App1() {
-  return (
-    <div className="App">
-      Melissa
-    </div>
-  );
-}
+// function App1() {
 
-export default App1;
+
+//   const [ flights, setFlights ] = useState([])
+//   const [ departure, setDeparture ] = useState('')
+//   const [ destination, setDestination ] = useState('')
+
+  
+//   // const url = 'https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2020&dateTo=12/12/2020&partner=picky&v=3'
+
+//   const searchFlights = async (e) => {
+//     // e.preventDefault()
+//     const url = `https://api.skypicker.com/flights?flyFrom=${departure}&to=${destination}&dateFrom=12/01/2021&dateTo=12/12/2021&partner=picky&v=3`
+//     try {
+//       const response = await fetch(url)
+//       const data = await response.json()
+//       const firstData = data.data.slice(0, 5)
+//       console.log(firstData)
+//       setFlights(firstData)
+//     }catch(err) {
+//       console.log(err)
+//     }
+//   }
+
+//   useEffect(() => {
+//     searchFlights();
+//     // console.log(departure)
+//     // console.log(destination)
+//   },[departure, destination])
+
+//   const selectDeparture = (e) => {
+//     setDeparture(
+//       e.target.value
+//     )
+//   } 
+
+//   const selectDestination = (e) => {
+//     setDestination(
+//       e.target.value
+//     )
+//   } 
+
+//   return (
+//     <div className="App">
+//       <div className="searchBar">
+//       <h1>Flight Search</h1>
+//       <form className="form" onSubmit={searchFlights}>
+//         <label className="label" htmlFor="departure">Departure</label>
+//           <select className="input" name="departure" onChange={selectDeparture}>
+//             <option>Choose your departure</option>
+//             <option value="PRG">Prague</option>
+//             <option value="TXL">Berlin</option>
+//             <option value="WAW">Warsaw</option>
+//             <option value="PED">Pardubice</option>
+//           </select>
+
+//         <label>Destination</label>
+//         <select className="input" name="destination" onChange={selectDestination}>
+//         <option>Choose your destination</option>
+//             <option value="VLC">Valencia</option>
+//             <option value="BCN">Barcelona</option>
+//             <option value="MAD">Madrid</option>
+//             <option value="MXP">Milano</option>
+//             <option value="AIA">Athens</option>
+//         </select>
+            
+//           <button className="button">search</button>
+//       </form>
+//       </div>
+    
+//       <div className="flight">
+//       <h3>Flight Details:</h3>
+//       <div className="flight-cards">{departure} to {destination}
+//       {flights.map (
+        
+//         (flight, i) => 
+//         <ul className="flight--list">
+//           <li>Option {i+1}</li>
+
+//           <li>Departure: {DateTime.fromMillis(flight.dTime * 1000).toFormat('hh:mm')}</li>
+          
+//           <li>Arrival: {DateTime.fromMillis(flight.aTime * 1000).toFormat('hh:mm')}</li>
+
+//           <li>Duration: {flight.fly_duration} </li>
+//         </ul>
+        
+//         )
+//       }
+//       </div>
+  
+      
+
+//   </div>
+      
+//     </div>
+//   );
+// }
+
+// export default App1;
+
