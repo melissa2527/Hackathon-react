c0131d7a8787d572a706e177cd4bde636f412e86
haianhxanh <hanka7n@gmail.com>
2020-11-06 12:30:44 +0100

    displaying

--- src/App2.js
+++ src/App2.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { DateTime } from 'luxon';
+import { DateTime, datetimeFromREST  } from 'luxon';
 import './App.css';
 
 function App2() {
@@ -9,28 +9,27 @@ function App2() {
   const [ destination, setDestination ] = useState('')
 
   
-
-
   // const url = 'https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2020&dateTo=12/12/2020&partner=picky&v=3'
 
   const searchFlights = async (e) => {
-    e.preventDefault()
+    // e.preventDefault()
     const url = `https://api.skypicker.com/flights?flyFrom=${departure}&to=${destination}&dateFrom=12/01/2021&dateTo=12/12/2021&partner=picky&v=3`
     try {
       const response = await fetch(url)
       const data = await response.json()
-      setFlights(data.data)
-      
+      const firstData = data.data.slice(0, 5)
+      console.log(firstData)
+      setFlights(firstData)
     }catch(err) {
       console.log(err)
     }
   }
 
-  // useEffect(() => {
-  //   searchFlights();
-  //   console.log(departure)
-  //   console.log(destination)
-  // },[departure, destination])
+  useEffect(() => {
+    searchFlights();
+    // console.log(departure)
+    // console.log(destination)
+  },[departure, destination])
 
   const selectDeparture = (e) => {
     setDeparture(
@@ -47,9 +46,10 @@ function App2() {
   return (
     <div className="App">
       Hanka
-      <form className="form">
+      <form className="form" onSubmit={searchFlights}>
         <label className="label" htmlFor="departure">Departure</label>
           <select name="departure" onChange={selectDeparture}>
+            <option>Choose your departure</option>
             <option value="PRG">Prague</option>
             <option value="TXL">Berlin</option>
             <option value="WAW">Warsaw</option>
@@ -58,24 +58,28 @@ function App2() {
 
         <label>Destination</label>
         <select name="destination" onChange={selectDestination}>
-            <option  value="VLC">Valencia</option>
-            <option  value="BCN">Barcelona</option>
-            <option  value="MAD">Madrid</option>
-            <option  value="MXP">Milano</option>
-            <option  value="AIA">Athens</option>
+        <option>Choose your destination</option>
+            <option value="VLC">Valencia</option>
+            <option value="BCN">Barcelona</option>
+            <option value="MAD">Madrid</option>
+            <option value="MXP">Milano</option>
+            <option value="AIA">Athens</option>
         </select>
             
-          <button className="button" onClick={searchFlights}>search</button>
+          <button className="button">search</button>
       </form>
 
       <div className="flight">
           <h3>Flight Details:</h3>
-          <p>Departure: {departure}</p>
-          <p>Destination: {destination}</p>
+          <p>Departure: {departure} - {flights.map(flight => DateTime.fromMillis(flight.dTime * 1000).toFormat('hh:mm'))}</p>
+          <p>Destination: {destination} - {flights.map(flight => DateTime.fromMillis(flight.aTime * 1000).toFormat('hh:mm'))}</p>
+          <p>Duration: {flights.map(flight => flight.fly_duration)}</p>
+          
+  
       </div>
-      {console.log(flights)}
     </div>
   );
 }
 
 export default App2;
+
