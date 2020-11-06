d823222c1679529759e904195b39752bfc997348
annie2527 <you@example.com>
2020-11-06 11:04:32 +0100

    changes

--- src/App2.js
+++ src/App2.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react'
-
-
+import { DateTime } from 'luxon';
 import './App.css';
 
 function App2() {
 
-  const [ flight, setFlight ] = useState([])
+  // const [ flight, setFlight ] = useState([])
   const [ departure, setDeparture ] = useState('')
   const [ destination, setDestination ] = useState('')
 
-  const url = 'https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2020&dateTo=12/12/2020&partner=picky&v=3'
+  
+
+
+  // const url = 'https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2020&dateTo=12/12/2020&partner=picky&v=3'
 
   const searchFlights = async (e) => {
     // e.preventDefault()
-
+    const url = `https://api.skypicker.com/flights?flyFrom=${departure}&to=${destination}&dateFrom=12/01/2021&dateTo=12/12/2021&partner=picky&v=3`
     try {
       const response = await fetch(url)
       const data = await response.json()
@@ -27,10 +29,17 @@ function App2() {
   useEffect(() => {
     searchFlights();
     console.log(departure)
+    console.log(destination)
   },[departure, destination])
 
   const selectDeparture = (e) => {
     setDeparture(
+      e.target.value
+    )
+  } 
+
+  const selectDestination = (e) => {
+    setDestination(
       e.target.value
     )
   } 
@@ -40,24 +49,30 @@ function App2() {
       Hanka
       <form className="form" onSubmit={searchFlights}>
         <label className="label" htmlFor="departure">Departure</label>
-          <select name="departure" onChange={selectDeparture}>
+          <select name="departure" onClick={selectDeparture}>
             <option value="PRG">Prague</option>
             <option value="TXL">Berlin</option>
             <option value="WAW">Warsaw</option>
-            <option value="">Pardubice</option>
+            <option value="PED">Pardubice</option>
           </select>
 
         <label>Destination</label>
-        <select>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
+        <select name="destination" onClick={selectDestination}>
+            <option value="VLC">Valencia</option>
+            <option value="BCN">Barcelona</option>
+            <option value="MAD">Madrid</option>
+            <option value="MXP">Milano</option>
+            <option value="AIA">Athens</option>
         </select>
             
           <button className="button">search</button>
       </form>
+
+      <div className="flight">
+          <h3>Flight Details:</h3>
+          <p>Departure: {departure}</p>
+          <p>Destination: {destination}</p>
+      </div>
     </div>
   );
 }
